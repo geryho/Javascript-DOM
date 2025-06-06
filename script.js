7212d858bdf8ba165ac06f3ed2984cc69c5d027e
Gery Ho <geryho88@gmail.com>
2025-06-06 16:47:40 +0700

    new files

---
+++ script.js
@@ -0,0 +1,21 @@
+//DOM Selection
+//document.getElementById()
+const judul = document.getElementById("judul");
+judul.style.color = "red";
+judul.style.backgroundColor = "black";
+judul.innerHTML = "Selamat Datang di Belajar DOM";
+
+//document.getElementsByTagName()
+const p = document.getElementsByTagName("p");
+for (let i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = "lightblue";
+}
+
+const h1 = document.getElementsByTagName("h1")[0];
+h1.style.fontSize = "30px";
+
+//document.getElementsByClassName()
+//->htmlCollection
+const p1 = document.getElementsByClassName("p1");
+p1[0].innerHTML = "Ini diubah dari JavaScript";
+console.log(2 + 2);
