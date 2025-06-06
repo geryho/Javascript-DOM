d35b1752a1b8cd0c361158dabbae852c41a77faa
Geryho <geryho88@gmail.com>
2025-06-06 23:43:32 +0700

    modified

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //DOM Selection
 //document.getElementById()
-const judul = document.getElementById("judul");
+//->element
+/*const judul = document.getElementById("judul");
 judul.style.color = "red";
 judul.style.backgroundColor = "black";
 judul.innerHTML = "Selamat Datang di Belajar DOM";
 
 //document.getElementsByTagName()
+//->htmlCollection
 const p = document.getElementsByTagName("p");
 for (let i = 0; i < p.length; i++) {
   p[i].style.backgroundColor = "lightblue";
@@ -18,4 +20,32 @@ h1.style.fontSize = "30px";
 //->htmlCollection
 const p1 = document.getElementsByClassName("p1");
 p1[0].innerHTML = "Ini diubah dari JavaScript";
-console.log(2 + 2);
+*/
+
+//document.querySelector()
+//->element
+
+/*const p4 = document.querySelector("#b p");
+
+p4.style.color = "green";
+p4.style.fontSize = "30px";
+
+const li2 = document.querySelector("section#b ul li:nth-child(2)");
+li2.style.backgroundColor = "Orange";
+*/
+
+//Jika memakai hanya querySelector maka yang terpilih hanya element p yang pertama, harus memakai querySelectorAll jika ingin memilih semua element p
+//const p = document.querySelector("p");
+//p.innerHTML = "Ini diubah melalui javascript";
+
+//document.querySelectorAll()
+//const p = document.querySelectorAll("p");
+//for (i = 0; i < p.length; i++) {
+//  p[i].style.backgroundColor = "lightblue";
+//}
+
+//bisa ditulis dengan
+
+const sectionB = document.getElementById("b");
+const p4 = sectionB.querySelector("p");
+p4.style.backgroundColor = "lightgreen";
